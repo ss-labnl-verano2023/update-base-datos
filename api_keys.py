291035f0617ea57ad962631f9a870ca1cafcd86f
Daniel Molina <117100281+Daniel-gmol@users.noreply.github.com>
2023-07-11 18:25:09 -0600

    Create api_keys.py
    Faltan llaves

---
+++ api_keys.py
@@ -0,0 +1,9 @@
+# Identificador (ID) documento GOOGLE SHEET propio a Trabajar
+# Aquí va el ID que se encunetra en el link del google sheet después de
+# https://docs.google.com/spreadsheets/d/{ID}
+SHEET_ID = '1jY2hmf-trZ9JvBAVLwnj3eOUCd_jiGgE_ySe4YJkDbU'
+
+
+# GOOGEL SHEETS API constant
+# Aqúi va el JSON con la llave del API
+KEY_FILE = ''
